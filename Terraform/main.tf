035316d7b7101403ea82b0ad9c36cb51ec20bf4a
Durga <cherukuladurga0707@gmail.com>
2025-03-26 19:28:11 +0530

    added 'kubernetes

--- Terraform/main.tf
+++
@@ -1,9 +0,0 @@
-provider "aws" {
-    region = "ap-south-1"
-}
-
-resource "aws_instance" "south_instance" {
- ami = ami-00bb6a80f01f03502
- instance_type = t2.micro
-  
-}
--- Terraform/modules/ec2_instances/main.tf
+++
@@ -1,9 +0,0 @@
-provider "aws" {
-    region = "ap-south-1"
-}
-
-resource "aws_instance" "south_instance" {
- ami = var.ami_value
- instance_type = var.instance_type_value
-  
-}
